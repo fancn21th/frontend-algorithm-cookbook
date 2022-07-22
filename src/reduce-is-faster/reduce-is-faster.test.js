722da264487ef3df0b752b0a84499695ea5f840e
fancn21th <fancn21th@gmail.com>
2022-07-22 16:12:39 +0800

    增加测试

--- src/reduce-is-faster/reduce-is-faster.test.js
+++ src/reduce-is-faster/reduce-is-faster.test.js
@@ -1,12 +1,22 @@
 import { filterMappedBigData, reducedBigData } from "./reduce-is-faster";
 
-const length = 10_000_000;
+const length = 1_000_000;
 
 // https://stackoverflow.com/questions/3746725/how-to-create-an-array-containing-1-n
 //=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ...]
+const smallData = Array.from({ length: 4 }, (_, i) => i + 1);
 const bigData = Array.from({ length }, (_, i) => i + 1);
 
 test("reduce function is faster than map & filter compose", () => {
+  const expected = [4, 8];
+  const filterMappedResult = filterMappedBigData(smallData);
+  const reducedResult = reducedBigData(smallData);
+
+  expect(filterMappedResult).toStrictEqual(expected);
+  expect(reducedResult).toStrictEqual(expected);
+
+  // 以下并非测试而是查看对比时间消耗
+
   console.time("filter and map");
   filterMappedBigData(bigData);
   console.timeEnd("filter and map");
@@ -14,6 +24,4 @@ test("reduce function is faster than map & filter compose", () => {
   console.time("reduce");
   reducedBigData(bigData);
   console.timeEnd("reduce");
-
-  return undefined;
 });
