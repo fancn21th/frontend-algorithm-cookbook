1cf0a26883bfda3d1ac90e3544cff609f798ec1b
fancn21th <fancn21th@gmail.com>
2022-07-23 09:05:46 +0800

    add test cases

--- src/nested-property/nested-property.test.js
+++ src/nested-property/nested-property.test.js
@@ -1,6 +1,6 @@
 import { getNestedPropByPath } from "./nested-property";
 
-const character = {
+const characterA = {
   name: "悟空",
   master: {
     name: "唐僧",
@@ -10,16 +10,34 @@ const character = {
   },
 };
 
+const characterB = {
+  name: "唐僧",
+  master: {
+    name: "观音",
+  },
+};
+
 test("access nested prop by path, and it works ", () => {
   const path = "master.master.name";
-  const result = getNestedPropByPath(path)(character);
+  const result = getNestedPropByPath(path)(characterA);
 
   expect(result).toBe("观音");
 });
 
 test("access nested prop by path, and it fails ", () => {
-  const path = "master.master.master.name";
-  const result = getNestedPropByPath(path)(character);
+  const path = "master.master.name";
+  const result = getNestedPropByPath(path)(characterB);
 
   expect(result).toBe(false);
+});
+
+test("access nested prop in array by path, and it works ", () => {
+  const path = "master.master.name";
+  const characters = [characterA, characterB];
+  const results = ["观音", false];
+
+  characters.forEach((c, index) => {
+    const value = getNestedPropByPath(path)(c);
+    expect(value).toBe(results[index]);
+  });
 });
