7ddc1b5f4c3ee3c04015d3cf3fc2d7348d43d4c0
fancn21th <fancn21th@gmail.com>
2022-09-22 10:26:08 +0800

    nodes in a row test case

--- src/reverse-sankey-data/reverse-sankey-data.test.js
+++ src/reverse-sankey-data/reverse-sankey-data.test.js
@@ -1,6 +1,6 @@
 import { reverseSankeyData } from "./reverse-sankey-data";
 
-test("single flow", () => {
+test("single flow with two nodes", () => {
   const source = [
     {
       source: "foo",
@@ -16,6 +16,31 @@ test("single flow", () => {
   });
 
   const expected = { 1: { foo: 10 }, 2: { bar: 10 } };
+
+  expect(result).toStrictEqual(expected);
+});
+
+test("single flow with three nodes in a row", () => {
+  const source = [
+    {
+      source: "foo",
+      target: "bar",
+      value: 10,
+    },
+    {
+      source: "bar",
+      target: "baz",
+      value: 20,
+    },
+  ];
+
+  const result = reverseSankeyData(source);
+
+  console.log({
+    result,
+  });
+
+  const expected = { 1: { foo: 10 }, 2: { bar: 30 }, 3: { baz: 20 } };
 
   expect(result).toStrictEqual(expected);
 });
