37d1d4dd4c82637d482b9872ea0ac292847fdc57
fancn21th <fancn21th@gmail.com>
2022-09-22 10:22:32 +0800

    sankey data first test case

--- src/reverse-sankey-data/reverse-sankey-data.js
+++ src/reverse-sankey-data/reverse-sankey-data.js
@@ -1,9 +1,68 @@
-export const reverseSankeyData = ([]) => {
-  return [
-    {
-      foo: 10,
-      bar: 20,
-      baz: 30,
-    },
-  ];
+export const reverseSankeyData = (flowsData) => {
+  const nodes = flowsData.reduce((acc, cur, index) => {
+    const { source, target, value } = cur;
+
+    // step 1: create a number of flows
+    //  each flows consist of nodes and linked in one direction
+    //  a1 -> b1 -> c1 | a1 -> b2 -> c3
+    // step 2:
+    //  group each node
+
+    /**
+     *  data structure
+     *      basically node is unique
+     *
+     *      node a1
+     *      'a1': {
+     *        name: 'a1'
+     *        sequence: 0,
+     *        next: [node b1, node b2]
+     *        value:
+     *      }
+     *      b1: node b1
+     *      {
+     *        name: 'b1'
+     *        sequence: 1,
+     *        next: [node c1]
+     *        value:
+     *      }
+     *
+     * */
+
+    const toNode = {
+      name: target,
+      sequence: 2,
+      next: null,
+      value,
+    };
+
+    acc[target] = toNode;
+
+    const fromNode = {
+      name: source,
+      sequence: 1,
+      next: [toNode],
+      value,
+    };
+
+    acc[source] = fromNode;
+
+    return acc;
+  }, {});
+
+  console.log({
+    nodes,
+  });
+
+  return Object.keys(nodes).reduce((acc, key) => {
+    const { name, sequence, value } = nodes[key];
+
+    if (!acc[sequence]) {
+      acc[sequence] = {};
+    }
+
+    acc[sequence][name] = value;
+
+    return acc;
+  }, {});
 };
--- src/reverse-sankey-data/reverse-sankey-data.test.js
+++ src/reverse-sankey-data/reverse-sankey-data.test.js
@@ -1,14 +1,21 @@
 import { reverseSankeyData } from "./reverse-sankey-data";
 
-test("sankey is coming", () => {
-  const source = [];
-  const result = reverseSankeyData(source);
-  const expected = [
+test("single flow", () => {
+  const source = [
     {
-      foo: 10,
-      bar: 20,
-      baz: 30,
+      source: "foo",
+      target: "bar",
+      value: 10,
     },
   ];
+
+  const result = reverseSankeyData(source);
+
+  console.log({
+    result,
+  });
+
+  const expected = { 1: { foo: 10 }, 2: { bar: 10 } };
+
   expect(result).toStrictEqual(expected);
 });
