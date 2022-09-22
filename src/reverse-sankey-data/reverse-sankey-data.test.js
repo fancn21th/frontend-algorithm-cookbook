e5baaa9652747c6847c9d529b6aad6d922103f94
fancn21th <fancn21th@gmail.com>
2022-09-22 14:49:44 +0800

    3*3*3 shuffled

--- src/reverse-sankey-data/reverse-sankey-data.test.js
+++ src/reverse-sankey-data/reverse-sankey-data.test.js
@@ -157,7 +157,7 @@ describe("do consider data is complete", () => {
 
   test("data is in a row", () => {
     /*
-      a - b - c
+      a(10) - b(10) - c(10)
     */
 
     const source = [
@@ -232,10 +232,10 @@ describe("do consider data is complete", () => {
   test("data is shuffled in two levels ", () => {
     /*
 
-a(5|5)\    /c(9)
-      \  / 
-       \ 
-b(4|6) / \d(11)
+      a(5|5)\    /c(9)
+            \  / 
+            \ 
+      b(4|6) / \d(11)
     
     */
 
@@ -269,6 +269,131 @@ b(4|6) / \d(11)
     });
 
     const expected = { 1: { a: 10, b: 10 }, 2: { c: 9, d: 11 } };
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("data is shuffled in three levels ", () => {
+    /*
+
+      a(1|2|3)  d(3(1|1|1))  g(6)
+              X             X  
+      b(1|2|3)  e(6(2|2|2))  h(6)
+              X             X 
+      c(1|2|3)  f(9(3|3|3))  i(6)
+    
+    */
+
+    const source = [
+      // a -> d,e,f
+      {
+        source: "a",
+        target: "d",
+        value: 1,
+      },
+      {
+        source: "a",
+        target: "e",
+        value: 2,
+      },
+      {
+        source: "a",
+        target: "f",
+        value: 3,
+      },
+      // b -> d,e,f
+      {
+        source: "b",
+        target: "d",
+        value: 1,
+      },
+      {
+        source: "b",
+        target: "e",
+        value: 2,
+      },
+      {
+        source: "b",
+        target: "f",
+        value: 3,
+      },
+      //c -> d,e,f
+      {
+        source: "c",
+        target: "d",
+        value: 1,
+      },
+      {
+        source: "c",
+        target: "e",
+        value: 2,
+      },
+      {
+        source: "c",
+        target: "f",
+        value: 3,
+      },
+      //d -> g,h,i
+      {
+        source: "d",
+        target: "g",
+        value: 1,
+      },
+      {
+        source: "d",
+        target: "h",
+        value: 1,
+      },
+      {
+        source: "d",
+        target: "i",
+        value: 1,
+      },
+      //e -> g,h,i
+      {
+        source: "e",
+        target: "g",
+        value: 2,
+      },
+      {
+        source: "e",
+        target: "h",
+        value: 2,
+      },
+      {
+        source: "e",
+        target: "i",
+        value: 2,
+      },
+      //f -> g,h,i
+      {
+        source: "f",
+        target: "g",
+        value: 3,
+      },
+      {
+        source: "f",
+        target: "h",
+        value: 3,
+      },
+      {
+        source: "f",
+        target: "i",
+        value: 3,
+      },
+    ];
+
+    const result = reverseSankeyData(source);
+
+    console.log({
+      result,
+    });
+
+    const expected = {
+      1: { a: 6, b: 6, c: 6 },
+      2: { d: 3, e: 6, f: 9 },
+      3: { g: 6, h: 6, i: 6 },
+    };
 
     expect(result).toStrictEqual(expected);
   });
